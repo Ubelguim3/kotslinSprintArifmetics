51a06fcac28f2829f5a4b237d3a3807434c893fe
Ubelguim3 <sakrivoshein@mail.ru>
2025-08-23 00:49:53 +0300

    KS-2-1 task completed

---
+++ src/main/kotlin/Lesson2/Lesson2_Task1.kt
@@ -0,0 +1,8 @@
+fun task1() {
+    val student1 = 3.0
+    val student2 = 4.0
+    val student3 = 3.0
+    val student4 = 5.0
+    val average = ((student4 + student3 + student2 + student1) / 4)
+    println(average)
+}
